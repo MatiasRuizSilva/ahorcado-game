67e7e8d7d6befbefd25f853beb097fcffce036c6
matiasruizsilva <ruizsilvamatias@gmail.com>
2023-04-24 19:51:08 -0300

    add app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+(function(){
+ 
+    var juego = {
+    palabra: "ALURA",
+    estado: 1,
+    adivinado: ['A','L'],
+    errado: ['B','J','K','C',]
+}
+
+var html = {
+    hombre: document.getElementById('hombre'),
+    adiviniado: document.querySelector('.adiviinado'),
+    errado: document.querySelector('.errado')
+}
+
+function dibujar(juego) {
+    
+}
+
+console.log(juego)
+
+}())
+
